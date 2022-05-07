da9ca078f80b618cf19f27f115b9b61313364826
stasik <stas960@gmail.com>
2022-05-07 18:24:18 +0300

    refactor project

---
+++ settings.py
@@ -0,0 +1,12 @@
+settings = {
+    'token': 'BAD_TOKEN',
+    'bot': 'BOT_NAME',
+    'id': 000000000000,
+    'prefix': 'PREFIX',
+    'NEWS_API_KEY': 'BAD_NEW_API_KEY'
+}
+
+try:
+    from settings_local import *
+except:
+    pass
