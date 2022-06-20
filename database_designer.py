3077a9f90abf2486ac2a1b06370b3262d8753240
Alex <gamesevolutionteam@gmail.com>
2022-06-21 00:07:30 +0300

    fix: Wrong names in foreign keys

--- database_designer.py
+++ database_designer.py
@@ -28,8 +28,8 @@ def initialize_database():
         item_id INT PRIMARY KEY,
         user_id INT NOT NULL,
         cost INT,
-        FOREIGN KEY (items) REFERENCES items(id) ON UPDATE CASCADE ON DELETE CASCADE
-        FOREIGN KEY (users) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (item_id) REFERENCES items(id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
     )""")
 
     conn.commit()
